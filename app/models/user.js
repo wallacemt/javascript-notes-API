3b183c9ccbae15a07869dbfb457166ce877e72cc
Wallace Santana <wallacesantanak0@gmail.com>
2024-07-24 20:57:29 -0300

    Adição do Bcrypt ao projeto

--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose")
+const bcrypt = require("bcrypt");
+
 
 let userSchema = new mongoose.Schema({
     name: String,
@@ -7,5 +9,19 @@ let userSchema = new mongoose.Schema({
     created_at: { type: Date, default: Date.now},
     updated_at: {type: Date, default: Date.now}
 });
+
+userSchema.pre('save', function(next){
+    if (this.isNew || this.isModified('password')){
+        bcrypt.hash(this.password, 10, (err, hashedPassword) => {
+            if(err) {
+                next(err);
+            }else{
+                this.password = hashedPassword;
+                next();
+            }
+        }
+        )
+    }
+})
 
 module.exports = mongoose.model("User", userSchema)
